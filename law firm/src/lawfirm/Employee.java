fe63d80625b5c070de2d160cff3c8dd9194dab5f
Kidane <kidanegidey2018@gmail.com>
2019-12-08 21:21:21 -0800

    Ex2 and Ex3

---
+++ law firm/src/lawfirm/Employee.java
@@ -0,0 +1,21 @@
+package lawfirm;
+
+public class Employee {
+	 public void applyForVacation() {
+	        System.out.println("Use the yellow vacation form.");
+	    }
+
+	    public void showHours() {
+	        System.out.println("I work 40 hours per week.");
+	    }
+
+	    public void showSalary() {
+	        System.out.println("My salary is $40,000.");
+	    }
+
+	    public void showVacation() {
+	        System.out.println("I receive 2 weeks vacation.");
+	    }
+	}
+
+
